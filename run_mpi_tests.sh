b7acfbc3b5d9a808117e06fd83c46a40766138dd
ymerta <ymertakan@gmail.com>
2025-06-12 10:07:44 +0300

    ..

--- run_mpi_tests.sh
+++ run_mpi_tests.sh
@@ -3,12 +3,16 @@
 # Derle
 mpicc -o randomforest main_mpi.c load_data.c tree/tree.c
 
+# Sonuç dosyasını temizle
+echo "Random Forest MPI Test Results" > results_mpi.txt
+
 # Denenecek çekirdek sayıları
-for np in 1 2 4 8
+for np in 16 32 64 128
 do
-    echo "====================================="
-    echo "Running with $np MPI process(es)"
-    echo "====================================="
-    mpirun -np $np ./randomforest
-    echo ""
+    echo "=====================================" | tee -a results_mpi.txt
+    echo "Running with $np MPI process(es)" | tee -a results_mpi.txt
+    echo "=====================================" | tee -a results_mpi.txt
+
+    mpirun -np $np ./randomforest | tee -a results_mpi.txt
+    echo "" | tee -a results_mpi.txt
 done
